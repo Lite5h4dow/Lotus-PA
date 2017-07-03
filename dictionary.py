84eb51896230ddb13bdce907afd7f2bd8114036d
Shadow-Dev <Lite5h4dow@users.noreply.github.com>
2017-07-03 23:08:39 +0100

    making sure everything is here

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+runSystem = ["run","launch","start","open"]
+programNames = ["atom","discord","steam"]
+greetings = ["hello","hey","heya","sup","hola"]
+shutdowns = ["sleep"]
+wake = ["lotus","wake"]
